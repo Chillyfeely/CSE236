2cfa0b13a70337c69b1a83b037767e9cff04932f
chillyfeely <vayselcacan@gmail.com>
2024-03-04 01:09:33 +0300

    re-commit

--- app.py
+++ app.py
@@ -1,48 +1,57 @@
-from flask import Flask, render_template, url_for, request, redirect
+from flask import Flask, render_template, url_for, request, redirect, flash
 from flask_sqlalchemy import SQLAlchemy
+from werkzeug.utils import secure_filename
 from datetime import datetime
+import os
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///test.db"
+app.config["SECRET_KEY"] = os.urandom(24)
+app.config["UPLOAD_FOLDER"] = "static/images"
 db = SQLAlchemy(app)
 
-class SentMail(db.Model):
+
+class Mail(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.String(200), nullable=False)
     date_sent = db.Column(db.DateTime, default=datetime.utcnow)
+    filename = db.Column(db.String(300))  # new column to store the filename
 
     def __repr__(self):
-        return '<SentMail %r>' % self.id
+        return "<SentMail %r>" % self.id
 
-@app.route('/', methods=['POST', 'GET'])
+
+with app.app_context():
+    db.create_all()
+
+
+@app.route("/", methods=["POST", "GET"])
 def index():
-    return render_template('index.html')
-
-# @app.route('/delete/<int:id>')
-# def delete(id):
-#     task_to_delete = Todo.query.get_or_404(id)
-
-#     try:
-#         db.session.delete(task_to_delete)
-#         db.session.commit()
-#         return redirect("/")
-#     except:
-#         return "There was a problem deleting that task"
-
-# @app.route("/update/<int:id>", methods=['GET', 'POST'])
-# def update(id):
-#     task = Todo.query.get_or_404(id)
-#     if request.method == 'POST':
-#         task.content = request.form['content']
-
-#         try:
-#             db.session.commit()
-#             return redirect('/')
-#         except:
-#             return "There was an issue updating your task"
-#     else:
-#         return render_template('update.html', task=task)
+    emails = Mail.query.order_by(Mail.date_sent).all()
+    return render_template("index.html", emails=emails)
 
 
-if __name__ == '__main__':
+@app.route("/send_email", methods=["POST"])
+def send_email():
+    message = request.form["message"]
+    file = request.files["file"]
+    filename = secure_filename(file.filename) if file else None
+
+    if file:
+        # Save the file to the upload folder
+        file.save(os.path.join(app.config["UPLOAD_FOLDER"], filename))
+
+    new_message = Mail(content=message, filename=filename)
+
+    try:
+        db.session.add(new_message)
+        db.session.commit()
+        flash("Email stored successfully")
+        return redirect(url_for("index"))
+    except:
+        flash("There was an issue storing the email")
+        return redirect(url_for("index"))
+
+
+if __name__ == "__main__":
     app.run(debug=True)
--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,59 +1,68 @@
-window.addEventListener('DOMContentLoaded', event => {
+window.addEventListener("DOMContentLoaded", (event) => {
+  // Toggle the side navigation
+  const sidebarToggle = document.body.querySelector("#sidebarToggle");
+  if (sidebarToggle) {
+    sidebarToggle.addEventListener("click", (event) => {
+      event.preventDefault();
+      document.body.classList.toggle("sb-sidenav-toggled");
+      localStorage.setItem(
+        "sb|sidebar-toggle",
+        document.body.classList.contains("sb-sidenav-toggled")
+      );
+    });
+  }
 
-    // Toggle the side navigation
-    const sidebarToggle = document.body.querySelector('#sidebarToggle');
-    if (sidebarToggle) {
-        sidebarToggle.addEventListener('click', event => {
-            event.preventDefault();
-            document.body.classList.toggle('sb-sidenav-toggled');
-            localStorage.setItem('sb|sidebar-toggle', document.body.classList.contains('sb-sidenav-toggled'));
-        });
-    }
+  // Store the IDs of the page content divs for different buttons
+  const pageContentIds = {
+    homeBtnx: "homeContent",
+    homeBtn: "homeContent",
+    homeBtn2: "homeContent",
+    dashboardBtn: "dashboardContent",
+    profileBtn: "profileContent",
+    profileBtn2: "profileContent",
+    mailBtn: ["sentEmail", "receivedEmail", "sendEmail"],
+    sendEmailBtn: "sendEmailContent",
+    sentEmailsBtn: "sentEmailsContent",
+    // ...add more here...
+  };
 
-    // Store the IDs of the page content divs for different buttons
-    const pageContentIds = {
-        'homeBtnx': 'homeContent', 
-        'homeBtn': 'homeContent', 
-        'homeBtn2': 'homeContent',
-        'dashboardBtn': 'dashboardContent',
-        'profileBtn': 'profileContent',
-        'profileBtn2': 'profileContent',
-        'mailBtn': ['sentMail', 'receivedMail', 'sendEmail'], 
-        // ...add more here...
-    };
-
-    // Add event listeners to the sidebar links
-    for (let linkId in pageContentIds) {
-        const link = document.body.querySelector('#' + linkId);
-        if (link) {
-            link.addEventListener('click', event => {
-                event.preventDefault();
-                // Hide all page content divs
-                for (let contentId in pageContentIds) {
-                    const contentDivs = Array.isArray(pageContentIds[contentId]) ? pageContentIds[contentId] : [pageContentIds[contentId]];
-                    for (let contentDivId of contentDivs) {
-                        const contentDiv = document.body.querySelector('#' + contentDivId);
-                        if (contentDiv) {
-                            contentDiv.style.display = 'none';
-                        }
-                    }
-                }
-                // Show the page content div(s) for the clicked link
-                const contentDivs = Array.isArray(pageContentIds[linkId]) ? pageContentIds[linkId] : [pageContentIds[linkId]];
-                for (let contentDivId of contentDivs) {
-                    const contentDiv = document.body.querySelector('#' + contentDivId);
-                    if (contentDiv) {
-                        contentDiv.style.display = 'block';
-                    }
-                }
-            });
+  // Add event listeners to the sidebar links
+  for (let linkId in pageContentIds) {
+    const link = document.body.querySelector("#" + linkId);
+    if (link) {
+      link.addEventListener("click", (event) => {
+        event.preventDefault();
+        // Hide all page content divs
+        for (let contentId in pageContentIds) {
+          const contentDivs = Array.isArray(pageContentIds[contentId])
+            ? pageContentIds[contentId]
+            : [pageContentIds[contentId]];
+          for (let contentDivId of contentDivs) {
+            const contentDiv = document.body.querySelector("#" + contentDivId);
+            if (contentDiv) {
+              contentDiv.style.display = "none";
+            }
+          }
         }
+        // Show the page content div(s) for the clicked link
+        const contentDivs = Array.isArray(pageContentIds[linkId])
+          ? pageContentIds[linkId]
+          : [pageContentIds[linkId]];
+        for (let contentDivId of contentDivs) {
+          const contentDiv = document.body.querySelector("#" + contentDivId);
+          if (contentDiv) {
+            contentDiv.style.display = "block";
+          }
+        }
+      });
     }
+  }
 
-    // Show the page content div for 'homeBtnx' when the page loads
-    const homeContentDiv = document.body.querySelector('#' + pageContentIds['homeBtnx']);
-    if (homeContentDiv) {
-        homeContentDiv.style.display = 'block';
-    }
-
+  // Show the page content div for 'homeBtnx' when the page loads
+  const homeContentDiv = document.body.querySelector(
+    "#" + pageContentIds["homeBtnx"]
+  );
+  if (homeContentDiv) {
+    homeContentDiv.style.display = "block";
+  }
 });
